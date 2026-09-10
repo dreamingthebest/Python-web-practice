7d65380061148cb5fe5bd00d79b01c45f2840d51
dreamingthebest <105431908+dreamingthebest@users.noreply.github.com>
2026-09-10 12:59:31 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 
 
-st.set_page_config(page_title="Python Practice Web",layout="wide")
+st.set_page_config(page_title="Python Practice Web",page_icon="✨",layout="wide")
 st.title("Python Practice")
 st.write("New line")
